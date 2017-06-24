64d10be038b5dbafa058c9936e456d7df1731c47
muzrea <muzrea@hotmail.com>
2017-06-24 15:13:17 +0800

    first commit

--- main/student.js
+++ main/student.js
@@ -5,6 +5,6 @@ module.exports = function Student(name,age,klass)
 	Person.call(this,name,age);
 	this.klass=klass;
 	this.introduce = function() {
-		return "My name is " + this.name+". I am "+this.age+" years old."+"I am a Student. I am at Class " + this.klass+".";
+		return "My name is " + this.name+". I am "+this.age+" years old.I am a Student. I am at Class " + this.klass+".";
 	}
 }
