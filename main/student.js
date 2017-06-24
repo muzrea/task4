75b7745b333306bbe33e5efd2c5fee1408395dd7
muzrea <muzrea@hotmail.com>
2017-06-24 15:35:00 +0800

    first commit

--- main/student.js
+++ main/student.js
@@ -2,9 +2,9 @@ let Person = require('./person')
 // Write your code here
 module.exports = function Student(name,age,klass)
 { 
-	Person.call(this,name,age);
 	this.klass=klass;
 	this.introduce = function() {
 		return "My name is " + this.name+". I am "+this.age+" years old.I am a Student. I am at Class " + this.klass+".";
 	}
 }
+Student.prototype = new Person;
