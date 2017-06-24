8f98779de5293d68963ebc20c354db205470265f
muzrea <muzrea@hotmail.com>
2017-06-24 14:13:49 +0800

    first commit

--- main/person.js
+++ main/person.js
@@ -1,2 +1,9 @@
 // Write your code here
-module.exports = Person
+module.exports = function Person(name,age)
+{
+	this.name=name;
+	this.age=age;
+	this.Introduce = function() {
+		alert("My name is" + this.name+". I am"+this.age+"years old.");
+	}
+}
--- main/student.js
+++ main/student.js
@@ -1,3 +1,10 @@
 let Person = require('./person')
 // Write your code here
-module.exports = Student
+module.exports = function Student(klass)
+{
+	person(name1,age1);
+	this.klass=klass;
+	this.Introduce = function() {
+		alert("I am a student. I am at class" + this.klass+".");
+	}
+}
