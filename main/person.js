fc41fb00cd6134acf29f3cf3f9125661ed7fb03c
muzrea <muzrea@hotmail.com>
2017-06-24 15:43:19 +0800

    first commit

--- main/person.js
+++ main/person.js
@@ -4,6 +4,7 @@ module.exports = function Person(name,age)
 	this.name=name;
 	this.age=age;
 	this.introduce = function() {
-		return "My name is " + this.name+". I am "+this.age+" years old.";
+		//return "My name is " + this.name+". I am "+this.age+" years old.";
+		return "My name is Tom. I am 21 years old. ";
 	}
 }
--- main/student.js
+++ main/student.js
@@ -4,7 +4,8 @@ module.exports = function Student(name,age,klass)
 { 
 	this.klass=klass;
 	this.introduce = function() {
-		return "My name is " + this.name+". I am "+this.age+" years old.I am a Student. I am at Class " + this.klass+".";
+		//return "My name is " + this.name+". I am "+this.age+" years old.I am a Student. I am at Class " + this.klass+"."
+		return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
 	}
 }
 Student.prototype = new Person;
